a37ee0beda8de001f57a854505e8a7805f448004
drgorillamd <dr_gorilla@protonmail.com>
2023-10-08 23:30:02 +0200

    feat: sen_request tests

--- src/test/e2e.test.ts
+++ src/test/e2e.test.ts
@@ -13,6 +13,7 @@ import {
   createDebugLogger,
   getSandboxAccountsWallets,
   waitForSandbox,
+  AztecAddress,
 } from "@aztec/aztec.js";
 import { toBigIntBE } from "@aztec/foundation/bigint-buffer";
 import { format } from "util";
@@ -24,9 +25,12 @@ const {
   ETHEREUM_HOST = "http://localhost:8545",
 } = process.env;
 
+const QUESTIONS_SLOT: Fr = new Fr(1);
+const ANSWERS_SLOT: Fr = new Fr(2);
+
 let pxe: PXE;
 let oracle: PrivateOracleContract;
-let requestor: AccountWallet;
+let requester: AccountWallet;
 let divinity: AccountWallet;
 
 beforeAll(async () => {
@@ -40,24 +44,73 @@ beforeAll(async () => {
 
   logger(format("Aztec Sandbox Info ", nodeInfo));
 
-  requestor = await createAccount(pxe);
+  requester = await createAccount(pxe);
   divinity = await createAccount(pxe);
 });
 
 describe("E2E Private Oracle", () => {
-  beforeEach(async () => {
+  // Deploy the oracle
+  beforeAll(async () => {
     const deployer = await createAccount(pxe);
 
     oracle = await PrivateOracleContract.deploy(deployer).send().deployed();
   }, 30_000);
 
-  it("send request", async () => {
-    const receipt = await oracle
-      .withWallet(requestor)
-      .methods.submit_question(123, divinity.getAddress())
-      .send()
-      .wait();
+  describe("submit_question(..)", () => {
+    let shared_key_nullifier_divinity: Fr;
+    let shared_key_nullifier_requester: Fr;
 
-    console.log(receipt);
+    it("Tx to submit_question is mined", async () => {
+      // Submit the question
+      const receipt = await oracle
+        .withWallet(requester)
+        .methods.submit_question(123, divinity.getAddress())
+        .send()
+        .wait();
+
+      expect(receipt.status).toBe("mined");
+    });
+
+    it("divinity note has the correct data", async () => {
+      const divinityNotes = await pxe.getPrivateStorageAt(
+        divinity.getAddress(),
+        oracle.address,
+        QUESTIONS_SLOT
+      );
+
+      expect(divinityNotes[0].items[0].value).toEqual(123n);
+      expect(AztecAddress.fromField(divinityNotes[0].items[1])).toEqual(
+        requester.getAddress()
+      );
+      expect(AztecAddress.fromField(divinityNotes[0].items[2])).toEqual(
+        divinity.getAddress()
+      );
+
+      shared_key_nullifier_divinity = divinityNotes[0].items[3];
+    });
+
+    it("requester note has the correct data", async () => {
+      const requesterNotes = await pxe.getPrivateStorageAt(
+        requester.getAddress(),
+        oracle.address,
+        QUESTIONS_SLOT
+      );
+
+      expect(requesterNotes[0].items[0].value).toEqual(123n);
+      expect(AztecAddress.fromField(requesterNotes[0].items[1])).toEqual(
+        requester.getAddress()
+      );
+      expect(AztecAddress.fromField(requesterNotes[0].items[2])).toEqual(
+        divinity.getAddress()
+      );
+
+      shared_key_nullifier_requester = requesterNotes[0].items[3];
+    });
+
+    it("nullifier key is the same between the 2 notes", async () => {
+      expect(shared_key_nullifier_divinity).toEqual(
+        shared_key_nullifier_requester
+      );
+    });
   });
 });

5d8a9af8fe406fbe506a514c10b6b841e3786669
shaito <106555513+0xShaito@users.noreply.github.com>
2023-12-04 13:15:39 +0100

    feat: unconstrained get questions and answers (#14)
    * feat: unconstrained get questions and answers
    
    * feat: test get answers - need bach tx
    
    * chore: batch tx and expect
    
    * feat: answer note type and quick refactor
    
    * chore: refactor
    
    * test: get questions
    
    * test: get_questions_unconstrained
    
    * chore: typo
    
    * chore: wip types
    
    * chore: tmp push for debugging
    
    * chore: more debug
    
    * feat: set_offset in array getters
    
    * chore: types and getters in tests
    
    * test: escrow sig to fix
    
    * fix: test
    
    * test: offset
    
    * fix: typo
    
    * chore: v bump
    
    * tests: types
    
    * fix: typed notes
    
    * fix: pxe register + note amounts
    
    * chore: wip offset test
    
    * chore: skip offset tests
    
    ---------
    
    Co-authored-by: drgorillamd <dr_gorilla@protonmail.com>

---
+++ types/Notes.ts
@@ -0,0 +1,69 @@
+import { AztecAddress, Fr } from "@aztec/aztec.js";
+
+export class AnswerNote {
+  request: bigint;
+  answer: bigint;
+  requester: AztecAddress;
+  divinity: AztecAddress;
+  owner: AztecAddress;
+
+  constructor(request: bigint, answer: bigint, requester: AztecAddress, divinity: AztecAddress, owner: AztecAddress) {
+    this.request = request;
+    this.answer = answer;
+    this.requester = requester;
+    this.divinity = divinity;
+    this.owner = owner;
+  }
+
+  static fromChainData(note: any) {
+    return new AnswerNote(
+      note.request,
+      note.answer,
+      AztecAddress.fromBigInt(note.requester.address),
+      AztecAddress.fromBigInt(note.divinity.address),
+      AztecAddress.fromBigInt(note.owner.address),
+    );
+  }
+
+  static fromLocal(note: any) {
+    return new AnswerNote(
+      note.request,
+      note.answer,
+      AztecAddress.fromBigInt(note.requester.asBigInt),
+      AztecAddress.fromBigInt(note.divinity.asBigInt),
+      AztecAddress.fromBigInt(note.owner.asBigInt),
+    );
+  }
+}
+
+export class QuestionNote {
+  request: bigint;
+  requester: AztecAddress;
+  divinity: AztecAddress;
+  shared_nullifier_key: bigint;
+
+  constructor(request: bigint, requester: AztecAddress, divinity: AztecAddress, shared_nullifier_key: bigint) {
+    this.request = request;
+    this.requester = requester;
+    this.divinity = divinity;
+    this.shared_nullifier_key = shared_nullifier_key;
+  }
+
+  static fromChainData(note: any) {
+    return new QuestionNote(
+      note.request,
+      AztecAddress.fromBigInt(note.requester_address.address),
+      AztecAddress.fromBigInt(note.divinity_address.address),
+      note.shared_nullifier_key,
+    );
+  }
+
+  static fromLocal(note: any) {
+    return new QuestionNote(
+      note.request,
+      AztecAddress.fromBigInt(note.requester_address.asBigInt),
+      AztecAddress.fromBigInt(note.divinity_address.asBigInt),
+      note.shared_nullifier_key,
+    );
+  }
+}
